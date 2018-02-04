70ce4a893f4446c20e8335c48f157868a38c81a8
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-02-04 12:26:15 +0100

    making the number of random tests adjustable from the command line

--- run_unit_tests.sh
+++ run_unit_tests.sh
@@ -5,5 +5,8 @@ if [ "$1" = "rebuild" ]; then
     (python setup.py build_ext --inplace --force)
 fi;
 
+
+### setting the number of random tests
+export RTC_COUNT="$2"
 (cd tests && python -m unittest discover -s . -v)
 
--- tests/test_random_subset_sum.py
+++ tests/test_random_subset_sum.py
@@ -1,17 +1,23 @@
 import unittest
 import array
 import random
+import os
 
 import pseudopol.cpseudopol as c_pp
 import pseudopol.ppseudopol as p_pp
 
 random.seed(42)
 
+try: 
+    RTC_COUNT=int(os.environ['RTC_COUNT'])
+except:
+    RTC_COUNT=20
+
 class RandomSubSetSumTester(unittest.TestCase): 
 
     def test_unique_weights(self):
         objects=array.array('I', random.sample(range(1,100), 20))
-        max_vals=random.sample(range(1,2000), 500)
+        max_vals=random.sample(range(1,2000), RTC_COUNT)
         for i in max_vals:
            c_result=c_pp.find_max_subsum(i, objects)
            p_result=p_pp.find_max_subsum(i, objects)
@@ -22,7 +28,7 @@ class RandomSubSetSumTester(unittest.TestCase):
         for i in range(500):
            lst.append(random.randint(1,100))
         objects=array.array('I', lst)
-        max_vals=random.sample(range(1,2000), 500)
+        max_vals=random.sample(range(1,2000), RTC_COUNT)
         for i in max_vals:
            c_result=c_pp.find_max_subsum(i, objects)
            p_result=p_pp.find_max_subsum(i, objects)
