4d39e31fe2dc5bd797a6935d9744b75150f3182c
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-02-04 12:16:14 +0100

    preparing setup for pip

--- setup.py
+++ setup.py
@@ -1,7 +1,37 @@
-from distutils.core import setup, Extension
+from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
+import warnings
 
-setup(ext_modules=cythonize(Extension(
+
+#for the time being only with cython:
+USE_CYTHON = True
+
+
+
+extensions = Extension(
             name='pseudopol.cpseudopol',
             sources = ["pseudopol/cpseudopol.pyx"],
-    )))
+    )
+
+if USE_CYTHON:
+    extensions = cythonize(extensions)
+
+kwargs = {
+      'name':'exetest',
+      'version':'0.1.0',
+      'description':'Collection of solution for pseudo-polynomial problems',
+      'author':'Egor Dranischnikow',
+      'url':'https://github.com/realead/exetest',
+      'packages':find_packages(),
+      'license': 'MIT',
+      'ext_modules':  extensions
+}
+
+try:
+    setup(**kwargs)
+except SystemExit:
+    del kwargs['ext_modules']
+    warnings.warn('compilation failed. Installing pure python package')
+setup(**kwargs)
+
+
