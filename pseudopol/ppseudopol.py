fe0c68a1eef885dd772e63b336a285d2ea1f7726
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-03-15 20:48:13 +0100

    adding python version for zero_subset_sum

--- pseudopol/ppseudopol.py
+++ pseudopol/ppseudopol.py
@@ -13,3 +13,36 @@ def find_max_subsum(max_sum, objects):
             return i
     
     return 0 
+
+
+
+
+
+def zerosum_subset_exists(objects):
+    if 0 in objects:
+        return True
+
+    positive=[x for x in objects if x>0]
+    negative=[abs(x) for x in objects if x<0]
+
+    ## all positive
+    n=sum(positive)
+    reachable=[False]*(n+1)
+    reachable[0]=True
+    for o in positive:
+        for i in range(n,-1,-1):
+            if reachable[i] and i+o<=n:
+                reachable[i+o]=True
+
+    ## all negative:
+    for o in negative:
+        for i in range(1,n+1):
+            if reachable[i]:
+                index=i-o
+                if index==0:
+                    return True
+                elif index>0:
+                    reachable[index]=True
+  
+    return False
+   
--- tests/unit_tests/test_zerosubset_sum.py
+++ tests/unit_tests/test_zerosubset_sum.py
@@ -4,87 +4,87 @@ import array
 import pseudopol.cpseudopol as c_pp
 import pseudopol.ppseudopol as p_pp
 
-class SubSetSumTester(object): 
+class ZeroSubSetSumTester(object): 
 
     def test_empty_set(self):
-        self.assertEqual(False, self.fun(array.array('i')))
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i')))
 
     def test_set_with_zero1(self):
-        self.assertEqual(True, self.fun(array.array('i',[0])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[0])))
 
     def test_set_with_zero2(self):
-        self.assertEqual(True, self.fun(array.array('i',[0,1])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[0,1])))
 
     def test_set_with_zero3(self):
-        self.assertEqual(True, self.fun(array.array('i',[2,0,1])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[2,0,1])))
 
     def test_set_with_zero4(self):
-        self.assertEqual(True, self.fun(array.array('i',[2,4,6,0])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[2,4,6,0])))
 
     def test_only_pos(self):
-        self.assertEqual(False, self.fun(array.array('i',[2,4,6])))
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[2,4,6])))
   
     def test_only_neg(self):
-        self.assertEqual(False, self.fun(array.array('i',[-2,-4,-6,-4,-8])))  
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[-2,-4,-6,-4,-8])))  
 
     def test_two_equal_pm_2(self):
-        self.assertEqual(True, self.fun(array.array('i',[2,-2])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[2,-2])))  
 
     def test_two_equal_pm_1(self):
-        self.assertEqual(True, self.fun(array.array('i',[-1,1]))) 
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-1,1]))) 
 
     def test_two_equal_pm_16(self):
-        self.assertEqual(True, self.fun(array.array('i',[-16,16])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-16,16])))
 
     def test_two_equal_pm_15(self):
-        self.assertEqual(True, self.fun(array.array('i',[-15,15])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-15,15])))  
 
     def test_two_equal_pm_31(self):
-        self.assertEqual(True, self.fun(array.array('i',[-31,31]))) 
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-31,31]))) 
 
     def test_two_equal_pm_32(self):
-        self.assertEqual(True, self.fun(array.array('i',[-32,32])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-32,32])))  
 
     def test_two_equal_pm_63(self):
-        self.assertEqual(True, self.fun(array.array('i',[-63,63]))) 
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-63,63]))) 
 
     def test_two_equal_pm_64(self):
-        self.assertEqual(True, self.fun(array.array('i',[-64,64]))) 
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-64,64]))) 
 
     def test_three_yes_1(self):
-        self.assertEqual(True, self.fun(array.array('i',[1,-2,1])))
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[1,-2,1])))
 
     def test_three_yes_2(self):
-        self.assertEqual(True, self.fun(array.array('i',[1,2,-3])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[1,2,-3])))  
 
     def test_three_yes_3(self):
-        self.assertEqual(True, self.fun(array.array('i',[1,63,-64])))    
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[1,63,-64])))    
 
     def test_three_yes_4(self):
-        self.assertEqual(True, self.fun(array.array('i',[-65,63,2])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-65,63,2])))  
 
     def test_three_no_1(self):
-        self.assertEqual(False, self.fun(array.array('i',[1,-3,1])))
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[1,-3,1])))
 
     def test_three_no_2(self):
-        self.assertEqual(False, self.fun(array.array('i',[1,4,-3])))  
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[1,4,-3])))  
 
     def test_three_no_3(self):
-        self.assertEqual(False, self.fun(array.array('i',[1,62,-64])))    
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[1,62,-64])))    
 
     def test_three_no_4(self):
-        self.assertEqual(False, self.fun(array.array('i',[-66,65,2])))   
+        self.assertEqual(False, self.pp.zerosum_subset_exists(array.array('i',[-66,65,2])))   
 
     def test_three_not_all_needed(self):
-        self.assertEqual(True, self.fun(array.array('i',[-64,64,2])))  
+        self.assertEqual(True, self.pp.zerosum_subset_exists(array.array('i',[-64,64,2])))  
 
 
 #real testers
-class CZeroSubsetSumTester(SubSetSumTester, unittest.TestCase):
-    fun=c_pp.zerosum_subset_exists
+class CZeroSubsetSumTester(ZeroSubSetSumTester, unittest.TestCase):
+    pp=c_pp
 
-#class PSubSetSumTester(SubSetSumTester, unittest.TestCase):
-#    pp=p_pp
+class PZeroSubsetSumTester(ZeroSubSetSumTester, unittest.TestCase):
+    pp=p_pp
 
 
 
